0abfd2cc36bd62fe4cb56adc1f2cbed73a962ee1
Benjamin <benmneb@gmail.com>
2021-06-12 00:53:27 +0800

    make it work with more suttas!

--- src/utils/handleSuttaLinkClick.js
+++ src/utils/handleSuttaLinkClick.js
@@ -1,7 +1,11 @@
 export function handleSuttaLinkClick(reference) {
-	const modifiedReference = /(AN|SN)[\s][0-9]{1,3}[.]|(MN|DN)/.test(reference)
-		? reference.toLowerCase().replace(/\s/g, '') + '/en/sujato'
-		: reference.toLowerCase().replace(/\s/g, '');
+	const translator = /^SNP/.test(reference) ? 'mills' : 'sujato';
+
+	const modifiedReference = /^(AN|SN)[\s][0-9]{1,3}[.]|^(MN|DN|ITI|DHP|SNP)/.test(
+		reference
+	)
+		? `${reference.toLowerCase().replace(/\s/g, '')}/en/${translator}`
+		: `${reference.toLowerCase().replace(/\s/g, '')}`;
 
 	return window.open(
 		`https://suttacentral.net/${modifiedReference}?ref=4NobleTruths`,
