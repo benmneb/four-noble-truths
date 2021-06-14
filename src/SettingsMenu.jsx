919567644592773d39380dd63e39bb7cb4421a9b
Benjamin <benmneb@gmail.com>
2021-06-14 15:51:25 +0800

    add SettingsMenuItem function

--- src/SettingsMenu.jsx
+++ src/SettingsMenu.jsx
@@ -1,41 +1,17 @@
 import { useContext, useState } from 'react';
 
-import {
-	Box,
-	FormControl,
-	IconButton,
-	Select,
-	Menu,
-	MenuItem,
-	Typography,
-	Tooltip
-} from '@material-ui/core';
+import { Box, IconButton, Menu, Tooltip } from '@material-ui/core';
 import { SettingsRounded } from '@material-ui/icons';
-import { makeStyles } from '@material-ui/core/styles';
 
+import SettingsMenuItem from './SettingsMenuItem';
 import { GlobalState } from './state/store';
 
-const useStyles = makeStyles((theme) => ({
-	menuItem: {
-		display: 'flex',
-		alignItems: 'center',
-		justifyContent: 'space-between',
-		margin: theme.spacing(1),
-		width: 366
-	},
-	formControlRoot: {
-		width: '48%'
-	}
-}));
-
 export default function SettingsMenu() {
-	const styles = useStyles();
-
 	const [anchorEl, setAnchorEl] = useState(null);
 
 	const [state, dispatch] = useContext(GlobalState);
 
-	function handleChange(event, source) {
+	function handleChangeTranslation(event, source) {
 		return dispatch({ type: `SET_${source}`, word: event.target.value });
 	}
 
@@ -69,96 +45,30 @@ export default function SettingsMenu() {
 				open={Boolean(anchorEl)}
 				onClose={handleCloseMenu}
 			>
-				<Box className={styles.menuItem}>
-					<Box>
-						<Typography component="span">
-							Translate{' '}
-							<Box component="span" fontStyle="italic">
-								samadhi
-							</Box>{' '}
-							as...{' '}
-						</Typography>
-					</Box>
-					<FormControl classes={{ root: styles.formControlRoot }}>
-						<Select
-							labelId="translate-samadhi-label"
-							id="translate-samadhi"
-							value={state.translations.samadhi}
-							onChange={(event) => handleChange(event, 'SAMADHI')}
-						>
-							<MenuItem value={'stillness'}>stillness</MenuItem>
-							<MenuItem value={'immersion'}>immersion</MenuItem>
-							<MenuItem value={'concentration'}>concentration</MenuItem>
-							<MenuItem value={'samadhi'}>samadhi</MenuItem>
-						</Select>
-					</FormControl>
-				</Box>
-				<Box className={styles.menuItem}>
-					<Box>
-						<Typography component="span">
-							Translate{' '}
-							<Box component="span" fontStyle="italic">
-								sankhara
-							</Box>{' '}
-							as...{' '}
-						</Typography>
-					</Box>
-					<FormControl classes={{ root: styles.formControlRoot }}>
-						<Select
-							labelId="translate-sankhara-label"
-							id="translate-sankhara"
-							value={state.translations.sankhara}
-							onChange={(event) => handleChange(event, 'SANKHARA')}
-						>
-							<MenuItem value={'volitional formations'}>volitional formations</MenuItem>
-							<MenuItem value={'choices'}>choices</MenuItem>
-						</Select>
-					</FormControl>
-				</Box>
-				<Box className={styles.menuItem}>
-					<Box>
-						<Typography component="span">
-							Translate{' '}
-							<Box component="span" fontStyle="italic">
-								vedana
-							</Box>{' '}
-							as...{' '}
-						</Typography>
-					</Box>
-					<FormControl classes={{ root: styles.formControlRoot }}>
-						<Select
-							labelId="translate-vedana-label"
-							id="translate-vedana"
-							value={state.translations.vedana}
-							onChange={(event) => handleChange(event, 'VEDANA')}
-						>
-							<MenuItem value={'sensation'}>sensation</MenuItem>
-							<MenuItem value={'feeling'}>feeling</MenuItem>
-						</Select>
-					</FormControl>
-				</Box>
-				<Box className={styles.menuItem}>
-					<Box>
-						<Typography component="span">
-							Translate{' '}
-							<Box component="span" fontStyle="italic">
-								namarupa
-							</Box>{' '}
-							as...{' '}
-						</Typography>
-					</Box>
-					<FormControl classes={{ root: styles.formControlRoot }}>
-						<Select
-							labelId="translate-namarupa-label"
-							id="translate-namarupa"
-							value={state.translations.namarupa}
-							onChange={(event) => handleChange(event, 'NAMARUPA')}
-						>
-							<MenuItem value={'mind and matter'}>mind and matter</MenuItem>
-							<MenuItem value={'name and form'}>name and form</MenuItem>
-						</Select>
-					</FormControl>
-				</Box>
+				<SettingsMenuItem
+					paliWord="samadhi"
+					englishOptions={['stillness', 'immersion', 'concentration']}
+					value={state.translations.samadhi}
+					handleChange={(event) => handleChangeTranslation(event, 'SAMADHI')}
+				/>
+				<SettingsMenuItem
+					paliWord="sankhara"
+					englishOptions={['volitional formations', 'choices']}
+					value={state.translations.sankhara}
+					handleChange={(event) => handleChangeTranslation(event, 'SANKHARA')}
+				/>
+				<SettingsMenuItem
+					paliWord="vedana"
+					englishOptions={['sensation', 'feeling']}
+					value={state.translations.vedana}
+					handleChange={(event) => handleChangeTranslation(event, 'VEDANA')}
+				/>
+				<SettingsMenuItem
+					paliWord="namarupa"
+					englishOptions={['mind and matter', 'name and form']}
+					value={state.translations.namarupa}
+					handleChange={(event) => handleChangeTranslation(event, 'NAMARUPA')}
+				/>
 			</Menu>
 		</Box>
 	);
