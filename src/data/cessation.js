1ad4b1a2d8a8f5ef47cb5897ddc72f033a9a6940
Benjamin <benmneb@gmail.com>
2021-06-26 17:22:29 +0800

    clean up + add grasping references

--- src/data/cessation.js
+++ src/data/cessation.js
@@ -2,18 +2,15 @@ export const cessation = {
 	truth: 'The Cessation of Suffering',
 	quotes: [
 		{
-			text:
-				'This is the noble truth of the cessation of suffering: It’s the remainderless cessation of craving.',
+			text: 'This is the noble truth of the cessation of suffering: It’s the remainderless cessation of craving.',
 			reference: 'SN 56.11'
 		},
 		{
-			text:
-				'When ignorance fades away and ceases with nothing left over... That is how this entire mass of suffering ceases.',
+			text: 'When ignorance fades away and ceases with nothing left over... That is how this entire mass of suffering ceases.',
 			reference: 'SN 12.1'
 		},
 		{
-			text:
-				'Trust me on this; be convinced. Have no doubts or uncertainties in this matter. Just this is the end of suffering.',
+			text: 'Trust me on this; be convinced. Have no doubts or uncertainties in this matter. Just this is the end of suffering.',
 			reference: 'SN 12.51'
 		}
 	],
@@ -22,51 +19,57 @@ export const cessation = {
 	tree: [
 		{
 			id: 'C-1',
-			text: 'When ignorance ceases, volitional formations cease'
+			text: 'The cessation of ignorance'
 		},
 		{
 			id: 'C-2',
-			text: 'When volitional formations cease, consciousness ceases'
+			text: 'The cessation of volitional formations'
 		},
 		{
 			id: 'C-3',
-			text: 'When consciousnesss ceases, mind and matter cease'
+			text: 'The cessation of consciousnesss'
 		},
 		{
 			id: 'C-4',
-			text: 'When mind and matter cease, the six sense bases cease'
+			text: 'The cessation of mind and matter'
 		},
 		{
 			id: 'C-5',
-			text: 'When the six sense bases cease, contact ceases'
+			text: 'The cessation of the six sense bases'
 		},
 		{
 			id: 'C-6',
-			text: 'When contact ceases, craving/thirst ceases'
+			text: 'The cessation of contact'
 		},
 		{
 			id: 'C-7',
-			text: 'When craving/thirst ceases, sensations/feelings cease'
+			text: 'The cessation of sensations'
 		},
 		{
 			id: 'C-8',
-			text: 'When sensations/feelings cease, grasping/taking up ceases'
+			text: 'The cessation of craving',
+			references: ['MN 75'],
+			elaboration:
+				'All the ascetics or brahmins of the past, future, or present who live rid of thirst, their minds peaceful inside, do so after truly understanding the origin, ending, gratification, drawback, and escape of sensual pleasures, and after giving up craving and dispelling passion for sensual pleasures.'
 		},
 		{
 			id: 'C-9',
-			text: 'When grasping/taking up ceases, becoming/continued existence ceases'
+			text: 'The cessation of grasping',
+			references: ['SN 12.52', 'MN 11'],
+			elaboration:
+				'Suppose a bonfire was burning with ten, twenty, thirty, or forty loads of wood. And no-one would toss in dry grass, cow dung, or wood from time to time. As the original fuel is used up and no more is added, the bonfire would be extinguished due to lack of fuel. In the same way, there are things that are prone to being grasped. When you concentrate on the drawbacks of these things, your craving ceases. When craving ceases, grasping ceases. When grasping ceases, continued existence ceases. When continued existence ceases, rebirth ceases. When rebirth ceases, old age and death, sorrow, lamentation, pain, sadness, and distress cease. That is how this entire mass of suffering ceases.'
 		},
 		{
 			id: 'C-10',
-			text: 'When becoming/continued existence ceases, rebirth ceases'
+			text: 'The cessation of continued existence'
 		},
 		{
 			id: 'C-11',
-			text: 'When rebirth ceases, old age and death cease'
+			text: 'The cessation of rebirth'
 		},
 		{
 			id: 'C-12',
-			text: 'When old age and death cease, suffering ceases'
+			text: 'The cessation of old age and death'
 		}
 	]
 };
