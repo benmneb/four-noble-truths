966a9130153c1db6c6ff4533bfa12ceea840789c
Benjamin <benmneb@gmail.com>
2021-07-30 14:35:30 +0800

    add getTimeAgo

--- app/src/utils/index.js
+++ app/src/utils/index.js
@@ -5,3 +5,5 @@ export { handleSuttaLinkClick } from './handleSuttaLinkClick';
 export { MinusSquare, PlusSquare, CloseSquare } from './Icons';
 
 export { Link } from './Link';
+
+export { getTimeAgo } from './timeAgo';
