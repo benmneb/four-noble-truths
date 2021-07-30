0e7c48709453687ec73e937e877c100c32910b41
Benjamin <benmneb@gmail.com>
2021-07-30 17:46:33 +0800

    isolate default elaborations as json

--- app/src/data/cessation.js
+++ app/src/data/cessation.js
@@ -1,8 +1,8 @@
 export const cessation = {
   text: 'The Cessation of Suffering',
-  elaboration:
+  explanation:
     'This is the noble truth of the cessation of suffering: It’s the fading away and cessation of that very same craving with nothing left over; giving it away, letting it go, releasing it, and not adhering to it.',
-  references: [
+  seeMore: [
     'SN 56.11',
     'SN 12',
     'SN 12.1',
@@ -17,9 +17,7 @@ export const cessation = {
     {
       id: 'C-1',
       text: 'The cessation of ignorance',
-      references: ['SN 12.1'],
-      elaboration:
-        'When ignorance fades away and ceases with nothing left over, choices cease. When choices cease, consciousness ceases. When consciousness ceases, name and form cease. When name and form cease, the six sense fields cease. When the six sense fields cease, contact ceases. When contact ceases, feeling ceases. When feeling ceases, craving ceases. When craving ceases, grasping ceases. When grasping ceases, continued existence ceases. When continued existence ceases, rebirth ceases. When rebirth ceases, old age and death, sorrow, lamentation, pain, sadness, and distress cease. That is how this entire mass of suffering ceases.',
+      seeMore: ['SN 12.1'],
     },
     {
       id: 'C-2',
@@ -48,16 +46,12 @@ export const cessation = {
     {
       id: 'C-8',
       text: 'The cessation of craving',
-      references: ['MN 75'],
-      elaboration:
-        'All the ascetics or brahmins of the past, future, or present who live rid of thirst, their minds peaceful inside, do so after truly understanding the origin, ending, gratification, drawback, and escape of sensual pleasures, and after giving up craving and dispelling passion for sensual pleasures.',
+      seeMore: ['MN 75'],
     },
     {
       id: 'C-9',
       text: 'The cessation of grasping',
-      references: ['SN 12.52', 'MN 11'],
-      elaboration:
-        'Suppose a bonfire was burning with ten, twenty, thirty, or forty loads of wood. And no-one would toss in dry grass, cow dung, or wood from time to time. As the original fuel is used up and no more is added, the bonfire would be extinguished due to lack of fuel. In the same way, there are things that are prone to being grasped. When you concentrate on the drawbacks of these things, your craving ceases. When craving ceases, grasping ceases. When grasping ceases, continued existence ceases. When continued existence ceases, rebirth ceases. When rebirth ceases, old age and death, sorrow, lamentation, pain, sadness, and distress cease. That is how this entire mass of suffering ceases.',
+      seeMore: ['MN 11'],
     },
     {
       id: 'C-10',
--- app/src/data/suffering.js
+++ app/src/data/suffering.js
@@ -2,104 +2,95 @@ export const suffering = {
   text: 'Suffering',
   explanation:
     'This is the noble truth of suffering: Rebirth is suffering; old age is suffering; illness is suffering; death is suffering; association with the disliked is suffering; separation from the liked is suffering; not getting what you wish for is suffering. In brief, the five grasping aggregates are suffering.',
-  references: ['SN 56.11', 'SN 56.13', 'SN 56.14', 'SN 35.28', 'AN 6.63'],
+  seeMore: ['SN 56.11', 'SN 56.13', 'SN 56.14', 'SN 35.28', 'AN 6.63'],
   children: [
     {
       id: 'S-1',
       text: 'Rebirth',
-      spokenBy: 'Ven. Sariputta',
     },
     {
       id: 'S-2',
       text: 'Old age',
-      spokenBy: 'Ven. Sariputta',
     },
     {
       id: 'S-3',
       text: 'Death',
-      spokenBy: 'Ven. Sariputta',
     },
     {
       id: 'S-4',
       text: 'Sorrow',
-      spokenBy: 'Ven. Sariputta',
     },
     {
       id: 'S-5',
       text: 'Lamentation',
-      spokenBy: 'Ven. Sariputta',
     },
     {
       id: 'S-6',
       text: 'Pain',
-      spokenBy: 'Ven. Sariputta',
     },
     {
       id: 'S-7',
       text: 'Sadness',
-      spokenBy: 'Ven. Sariputta',
     },
     {
       id: 'S-8',
       text: 'Distress',
-      spokenBy: 'Ven. Sariputta',
     },
     {
       id: 'S-9',
       text: 'Not getting what you wished for',
-      spokenBy: 'Ven. Sariputta',
     },
     {
       id: 'S-10',
       text: 'The five grasping aggregates',
-      references: ['SN 22.104', 'SN 22'],
+      seeMore: ['SN 22.104', 'SN 22'],
       children: [
         {
           id: 'S-10.1',
           text: 'Form',
-          references: ['SN 22.57', 'MN 28'],
+          seeMore: ['SN 22.57', 'MN 28'],
           children: [
             {
               id: 'S-10.1.1',
               text: 'Earth element',
-              references: ['MN 62'],
+              seeMore: ['MN 62'],
             },
             {
               id: 'S-10.1.2',
               text: 'Water element',
-              references: ['MN 62'],
+              seeMore: ['MN 62'],
             },
             {
               id: 'S-10.1.3',
               text: 'Fire element',
-              references: ['MN 62'],
+              seeMore: ['MN 62'],
             },
             {
               id: 'S-10.1.4',
               text: 'Air element',
-              references: ['MN 62'],
+              seeMore: ['MN 62'],
             },
           ],
         },
         {
           id: 'S-10.2',
           text: 'Sensation/feeling',
-          references: ['SN 22.57'],
+          seeMore: ['SN 22.57'],
         },
         {
           id: 'S-10.3',
           text: 'Perception',
-          references: ['SN 22.57'],
+          seeMore: ['SN 22.57'],
         },
         {
           id: 'S-10.4',
           text: 'Volitional formations',
-          references: ['SN 22.57'],
+          seeMore: ['SN 22.57'],
         },
         {
           id: 'S-10.5',
           text: 'Consciousness',
-          references: ['SN 22.57'],
+          seeMore: ['SN 22.57'],
         },
       ],
     },
