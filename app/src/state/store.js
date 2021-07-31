cff30ae825d67d4145d66a6177bf53436fb0a783
Benjamin <benmneb@gmail.com>
2021-07-31 18:26:39 +0800

    add redux-thunk and redux-devtools-extension

--- app/src/state/store.js
+++ app/src/state/store.js
@@ -1,11 +1,11 @@
-import { createStore, compose } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
+import thunk from 'redux-thunk';
 
 import { reducer } from './reducer';
 
-const enhancer =
-  process.env.NODE_ENV === 'development' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-    : compose;
-
-export const store = createStore(reducer, enhancer);
+export const store = createStore(
+  reducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
