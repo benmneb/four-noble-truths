4e1b8a54de0dfc1393719e08380535decfaf5f67
Benjamin <benmneb@gmail.com>
2021-08-02 21:02:26 +0800

    add CC0 logo to footer and improve styling

--- app/src/components/Footer.jsx
+++ app/src/components/Footer.jsx
@@ -5,11 +5,12 @@ import { makeStyles } from '@material-ui/core/styles';
 import { useDispatch } from 'react-redux';
 
 import { toggleContact } from '../state';
-import { Link } from '../utils';
+import { CC0, Link } from '../utils';
 
 const useStyles = makeStyles((theme) => ({
-  root: {
+  footer: {
     display: 'flex',
+    flexDirection: 'column',
     alignItems: 'center',
     justifyContent: 'center',
     padding: theme.spacing(2, 2, 1, 2),
@@ -24,41 +25,42 @@ export default function Footer() {
   const dispatch = useDispatch();
 
   return (
-    <Box className={styles.root} component="footer">
-      <Typography component="div">
-        <Link
-          footerLink
+    <Box className={styles.footer} component="footer">
+      <Box component="div" display="flex" justifyContent="center">
+        <Typography>
+          <Link
+            footerLink
+            href="https://github.com/benmneb"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            benmneb
+          </Link>
+        </Typography>
+      </Box>
+      <Box display="flex" justifyContent="space-around">
+        <Tooltip title="Contact" placement="left">
+          <IconButton onClick={() => dispatch(toggleContact())}>
+            <MailOutlineRounded />
+          </IconButton>
+        </Tooltip>
+        <IconButton
           rel="license noopener noreferrer"
           href="http://creativecommons.org/publicdomain/zero/1.0/"
           target="_blank"
         >
-          CC0
-        </Link>{' '}
-        <Link
-          footerLink
-          href="https://github.com/benmneb"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          benmneb
-        </Link>
-        <Box display="flex" justifyContent="space-around">
-          <Tooltip title="Contact" placement="left">
-            <IconButton onClick={() => dispatch(toggleContact())}>
-              <MailOutlineRounded />
-            </IconButton>
-          </Tooltip>
-          <Tooltip title="View source" placement="right">
-            <IconButton
-              href="https://github.com/benmneb/four-noble-truths"
-              target="_blank"
-              rel="noopener"
-            >
-              <GitHub />
-            </IconButton>
-          </Tooltip>
-        </Box>
-      </Typography>
+          <CC0 />
+        </IconButton>
+        <Tooltip title="View source" placement="right">
+          <IconButton
+            href="https://github.com/benmneb/four-noble-truths"
+            target="_blank"
+            rel="noopener"
+          >
+            <GitHub />
+          </IconButton>
+        </Tooltip>
+      </Box>
     </Box>
   );
 }
--- app/src/utils/index.js
+++ app/src/utils/index.js
@@ -4,3 +4,4 @@ export { MinusSquare, PlusSquare, CloseSquare } from './Icons';
 export { Link } from './Link';
 export { getTimeAgo } from './timeAgo';
 export { default as LoadingButton } from './LoadingButton';
+export { default as CC0 } from './CC0';
