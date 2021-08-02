20c0d0d42fd3d454922a908fcc97a54ed29e50a5
Benjamin <benmneb@gmail.com>
2021-08-02 09:42:26 +0800

    remove addMany end update

--- api/router.js
+++ api/router.js
@@ -1,11 +1,9 @@
 import express from 'express';
 
-import { get, add, addMany, update, remove } from './controllers.js';
+import { get, add, remove } from './controllers.js';
 
 export const router = express.Router();
 
 router.get('/:for', get);
 router.post('/', add);
-router.post('/many/', addMany);
-router.put('/:id', update);
 router.delete('/:id', remove);
