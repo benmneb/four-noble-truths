812a686e0b51870904d28a2887a84ef8961c46b5
benmneb <62878664+benmneb@users.noreply.github.com>
2024-11-13 19:46:44 +0700

    feat: remove license link and all tooltips

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,6 @@
-import { Box, IconButton, Tooltip } from '@material-ui/core';
+import { Box, IconButton } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import { GitHub } from '@material-ui/icons';
-
-import { CC0 } from '../utils';
 
 const useStyles = makeStyles((theme) => ({
   footer: {
@@ -23,24 +21,13 @@ export default function Footer() {
   return (
     <Box className={styles.footer} component="footer">
       <Box display="flex" justifyContent="space-around">
-        <Tooltip title="License" placement="left">
-          <IconButton
-            rel="license noopener noreferrer"
-            href="http://creativecommons.org/publicdomain/zero/1.0/"
-            target="_blank"
-          >
-            <CC0 />
-          </IconButton>
-        </Tooltip>
-        <Tooltip title="View source" placement="right">
-          <IconButton
-            href="https://github.com/benmneb/four-noble-truths"
-            target="_blank"
-            rel="noopener"
-          >
-            <GitHub />
-          </IconButton>
-        </Tooltip>
+        <IconButton
+          href="https://github.com/benmneb/four-noble-truths"
+          target="_blank"
+          rel="noopener"
+        >
+          <GitHub />
+        </IconButton>
       </Box>
     </Box>
   );
