79f1ac08acd6b6464c1cc9c5b2abe24f50b5ef93
benmneb <62878664+benmneb@users.noreply.github.com>
2024-11-13 18:21:14 +0700

    fix: remove the tooltip on `TooltipChip`

--- src/utils/TooltipChip.jsx
+++ src/utils/TooltipChip.jsx
@@ -1,4 +1,4 @@
-import { Chip, Tooltip } from '@material-ui/core';
+import { Chip } from '@material-ui/core';
 
 import { makeStyles } from '@material-ui/core/styles';
 
@@ -7,7 +7,7 @@ import { handleSuttaLinkClick } from './index';
 
 const useStyles = makeStyles((theme) => ({
   chip: {
-    padding: theme.spacing(1, 0),
+    padding: theme.spacing(2, 0),
     margin: theme.spacing(1, 0, 0, 1),
   },
 }));
@@ -18,13 +18,13 @@ export function TooltipChip({ sutta }) {
   const themeType = useStore((state) => state.themeType);
 
   return (
-    <Tooltip arrow title="Read on SuttaCentral.net">
-      <Chip
-        className={styles.chip}
-        variant={themeType === 'dark' ? 'outlined' : 'default'}
-        label={sutta}
-        onClick={() => handleSuttaLinkClick(sutta)}
-      />
-    </Tooltip>
+    // <Tooltip arrow title="Read on SuttaCentral.net">
+    <Chip
+      className={styles.chip}
+      variant={themeType === 'dark' ? 'outlined' : 'default'}
+      label={sutta}
+      onClick={() => handleSuttaLinkClick(sutta)}
+    />
+    // </Tooltip>
   );
 }
