3d5b6811e5a9a7c4f34291831e5295d247806895
benmneb <62878664+benmneb@users.noreply.github.com>
2024-12-02 06:56:42 +0100

    feat: change default theme to `'light'`

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,7 @@ import { create } from 'zustand';
 
 export const useStore = create((set) => ({
   showAboutDrawer: false,
-  themeType: 'dark',
+  themeType: 'light',
   snackPack: [],
 
   toggleAboutDrawer: () =>
